53da97df9dce26e614e9bce0915322418f124b6d
Luiz Omena <83364189+luizfilipeyep@users.noreply.github.com>
2025-05-06 10:44:13 -0300

    api config

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontSize: 190
   },
+  texto: {
+    fontSize: 90,
+  }
 });
